4812f1eccfed0f736d71e48dff315099dd6d026b
Ardian <ardian@naturallyfunny.dev>
2026-06-29 17:30:24 +0700

    chore: point redirects to renamed repos (drop -go suffix)

--- modules.js
+++ modules.js
@@ -1,9 +1,9 @@
 export const GITHUB_URL = "https://github.com/naturallyfunny";
 
 export const MODULES = {
-    adk: `${GITHUB_URL}/adk-go`,
-    api: `${GITHUB_URL}/api-go`,
-    postera: `${GITHUB_URL}/postera-go`,
+    adk: `${GITHUB_URL}/adk`,
+    api: `${GITHUB_URL}/api`,
+    postera: `${GITHUB_URL}/postera`,
     spotify: `${GITHUB_URL}/spotify`,
     tuya: `${GITHUB_URL}/tuya`
 };
