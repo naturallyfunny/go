2205d7e9fa8c03dbdf1d8f303166b32adb5ae65e
Ardian <ardian@naturallyfunny.dev>
2026-07-24 20:08:31 +0700

    feat: keep the old adk module for awhile

--- modules.js
+++ modules.js
@@ -1,10 +1,11 @@
 export const GITHUB_URL = "https://github.com/naturallyfunny";
 
 export const MODULES = {
-  agentkit: `${GITHUB_URL}/agentkit`,
+  adk: `${GITHUB_URL}/adk`,
   api: `${GITHUB_URL}/api`,
   postera: `${GITHUB_URL}/postera`,
   spotify: `${GITHUB_URL}/spotify`,
   tuya: `${GITHUB_URL}/tuya`,
   gworkspace: `${GITHUB_URL}/gworkspace`,
+  agentkit: `${GITHUB_URL}/agentkit`,
 };
